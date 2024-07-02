6e2724d80b9791e7fa72b5137537f96ce914eff8
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2024-07-02 20:30:24 +0900

    prettierでフォーマット

--- static/exercise.js
+++ static/exercise.js
@@ -1,10 +1,12 @@
-document.getElementById('show-answer-button').addEventListener('click', function() {
-  const answer = document.getElementById('answer-text');
-  if (answer.style.display === 'none') {
-    answer.style.display = 'block';
-    this.textContent = '解答を隠す';
-  } else {
-    answer.style.display = 'none';
-    this.textContent = '解答を表示';
-  }
-});
+document
+  .getElementById("show-answer-button")
+  .addEventListener("click", function () {
+    const answer = document.getElementById("answer-text");
+    if (answer.style.display === "none") {
+      answer.style.display = "block";
+      this.textContent = "解答を隠す";
+    } else {
+      answer.style.display = "none";
+      this.textContent = "解答を表示";
+    }
+  });
